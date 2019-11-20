529b330776123a3ad1e42e7df6ac32f2ed17f1f3
rdrew <robdrewupei@gmail.com>
2019-11-20 12:35:40 -0400

    fixed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,32 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var server       = require('browser-sync').create();
+var runSequence  = require('run-sequence');
+
+gulp.task('server', ['sass:dev'], function() {
+    server.init({
+        serveStatic: ['.'],
+        proxy: 'https://islandimagined.dev.islandarchives.ca',
+        startPath: 'user',
+        injectChanges: true,
+        files: ['css/ia-islandimagined.styles.css','js/ia-islandimagined.behaviors.js'],
+        plugins: ['bs-rewrite-rules'],
+        rewriteRules: [
+            {
+                match: 'https://islandimagined.dev.islandarchives.ca/sites/islandimagined.dev.islandarchives.ca/themes/ia_islandimagined/css/ia-islandimagined.styles.css',
+                replace: '/css/ia-islandimagined.styles.css',
+            },
+            {
+                match: ' https://islandimagined.dev.islandarchives.ca/sites/islandimagined.dev.islandarchives.ca/themes/ia_islandimagined/js/ia-islandimagined.behaviors.js',
+                replace: '/js/ia-islandimagined.behaviors.js',
+            },
+        ],
+    });
+});
 
 gulp.task('sass:prod', function () {
   gulp.src('./sass/*.scss')
@@ -22,11 +45,31 @@ gulp.task('sass:dev', function () {
       browsers: ['last 2 version']
     }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(server.stream());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass:dev']);
+// ##################
+// Watch Task
+// ##################
+
+gulp.task('watch', ['server', 'sass:dev'], function() {
+    gulp.watch('./sass/**/*.scss', ['sass:dev']);
 });
 
-gulp.task('default', ['sass:dev', 'sass:watch']);
+// ##################
+// Build Task
+// ##################
+
+gulp.task('build', function() {
+    runSequence(['sass:dev']);
+});
+
+// ##################
+// Default Task
+// ##################
+
+gulp.task('default', function() {
+    runSequence(['build', 'server', 'watch']);
+});
+
